c120dae3e4517c0746dbb262ac65510a1ecd071a
FSDonnelly <donnelly_frank@yahoo.com>
2019-10-23 23:18:37 -0700

    done

---
+++ js/script.js
@@ -0,0 +1,38 @@
+$(document).ready(() => {
+  let addButton = $('.add-button');
+  let field = $('.add-input');
+  let tasks = $('.tasks');
+  // Add Task
+  addButton.on('click', () => {
+    let elem = `<li class="task">
+            <div class="task-checked col-1">
+              <input type="checkbox" />
+            </div>
+            <div class="task-text col-2">${field.val()}</div>
+            <div class="task-remove col-3"></div>
+          </li>`;
+    tasks.append(elem);
+    field.val('');
+  });
+
+  // Remove Task
+  tasks.on('click', '.task-remove', function() {
+    $(this)
+      .parent()
+      .remove();
+  });
+
+  // Checked
+  tasks.on('change', '.task-checked input', function() {
+    $(this)
+      .parent()
+      .next()
+      .toggleClass('checked');
+  });
+
+  // Sortable Task
+  $(function() {
+    $('#sortable').sortable();
+    $('#sortable').disableSelection();
+  });
+});
